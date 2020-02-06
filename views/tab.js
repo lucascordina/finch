b88516554bb03b0e5cf8e69d1bc98a07e8fddd1a
Lucas Cordina <lucas.cordina92@gmail.com>
2020-02-06 07:40:55 +0100

    tech: species data extracting script

--- views/tab.js
+++ views/tab.js
@@ -1,34 +1,47 @@
 import { Species } from '../species/species.js';
     
-    
-var xobj = new XMLHttpRequest();
-xobj.overrideMimeType("application/json");
-xobj.open('GET', '../assets/data/occurrencedata.json', true);
-xobj.onreadystatechange = function () {
-    if (xobj.readyState == 4 && xobj.status == "200") {
-    var occurrencedata = (JSON.parse(xobj.responseText));
-    console.log(occurrencedata);
-    var uniqueSpecies = [];
-    var uniqueSpeciesReal = [];
-    occurrencedata.forEach(occurrence => {
-        var isUnique = true;
-        uniqueSpecies.forEach(uniqueSpec => {
-            if(uniqueSpec == occurrence.acceptedScientificName) {
-                isUnique = false;
-            }
-        });
+// OCCURRENCE DATA LOAD FROM JSON    
+// var xobj = new XMLHttpRequest();
+// xobj.overrideMimeType("application/json");
+// xobj.open('GET', '../assets/data/occurrencedata.json', true);
+// xobj.onreadystatechange = function () {
+//     if (xobj.readyState == 4 && xobj.status == "200") {
+//     var occurrencedata = (JSON.parse(xobj.responseText));
+//     console.log(occurrencedata);
+//     var uniqueSpecies = [];
+//     var uniqueSpeciesReal = [];
+//     occurrencedata.forEach(occurrence => {
+//         var isUnique = true;
+//         uniqueSpecies.forEach(uniqueSpec => {
+//             if(uniqueSpec == occurrence.acceptedScientificName) {
+//                 isUnique = false;
+//             }
+//         });
 
-        if(isUnique) {
+//         if(isUnique) {
 
-    console.log('ey yo');
-            uniqueSpeciesReal.push(new Species(
-                occurrence.gbifID, occurrence.kingdom, occurrence.phylum, occurrence.class, occurrence.order, occurrence.family, occurrence.genus, occurrence.subgenus
-            ));
+//     console.log('ey yo');
+//             uniqueSpeciesReal.push(new Species(
+//                 occurrence.gbifID, occurrence.kingdom, occurrence.phylum, occurrence.class, occurrence.order, occurrence.family, occurrence.genus, occurrence.subgenus
+//             ));
 
-            uniqueSpecies.push(occurrence.acceptedScientificName);
-        }
-    });
-    console.log(uniqueSpeciesReal);
-    }
-};
-xobj.send(null);
+//             uniqueSpecies.push(occurrence.acceptedScientificName);
+//         }
+//     });
+//     console.log(JSON.stringify(uniqueSpeciesReal));
+//     }
+// };
+// xobj.send(null);
+
+
+//SPECIES DATA LOAD FROM JSON
+// var xobj = new XMLHttpRequest();
+// xobj.overrideMimeType("application/json");
+// xobj.open('GET', '../assets/data/speciesdata.json', true);
+// xobj.onreadystatechange = function () {
+//     if (xobj.readyState == 4 && xobj.status == "200") {
+//         var speciesdata = (JSON.parse(xobj.responseText));
+//         console.log(speciesdata);
+//     }
+// };
+// xobj.send(null);
