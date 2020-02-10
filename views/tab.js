ae2e699d5aecd9213a082e7213481d887ecb4b93
Lucas Cordina <lucas.cordina92@gmail.com>
2020-02-10 18:30:53 +0100

    feat: remove attribution or description section based on data availability

--- views/tab.js
+++ views/tab.js
@@ -9,17 +9,42 @@ todaysSpecies.registerListener(function(val){
     document.getElementById("species-name").textContent = val.species;
     document.getElementById("order-name").textContent = val.order;
     document.getElementById("family-name").textContent = val.family;
-    document.getElementById("photo-author").textContent = val.imageLicenseHolder;
-    document.getElementById("license").textContent = val.imageLicenseType;
-    document.getElementById("species-extract").innerHTML = `${val.wikipediaDescription} <a href="${val.wikipediaUrl}" target="_blank">Read More</a>`;
-    document.getElementById("license").href = "https://creativecommons.org/licenses/by/3.0/";
+    
+    if(val.wikipediaDescription !== undefined && val.wikipediaDescription !== "" && val.wikipediaDescription !== null) {
+        document.getElementById("species-extract").innerHTML = `${val.wikipediaDescription} <a href="${val.wikipediaUrl}" target="_blank">Read More</a>`;
+    }
+
+    if(val.imageLicenseHolder == "" || val.imageLicenseHolder == null || val.imageLicenseHolder == undefined) {
+        document.getElementById("attribution-section").classList.add('hidden');
+    } else {
+        document.getElementById("photo-author").textContent = val.imageLicenseHolder;
+        document.getElementById("attribution-section").classList.remove('hidden');
+    }
+    if(val.imageLicenseType == "CC 2.0") {
+        document.getElementById("copyright-span").classList.remove('hidden');
+        document.getElementById("license").href = "https://creativecommons.org/licenses/by/2.0/";
+        document.getElementById("license").textContent = val.imageLicenseType;
+    } else if(val.imageLicenseType == "CC 3.0") {
+        document.getElementById("copyright-span").classList.remove('hidden');
+        document.getElementById("license").href = "https://creativecommons.org/licenses/by/3.0/";
+        document.getElementById("license").textContent = val.imageLicenseType;
+    } else if(val.imageLicenseType == "CC 4.0") {
+        document.getElementById("copyright-span").classList.remove('hidden');
+        document.getElementById("license").href = "https://creativecommons.org/licenses/by/4.0/";
+        document.getElementById("license").textContent = val.imageLicenseType;
+    } else {
+        if(document.getElementById("copyright-span") != null) {
+            document.getElementById("copyright-span").classList.add('hidden');
+        }
+    }
+    
 });
 
-// CODE USED FOR CURATING
-// document.addEventListener('keypress', nextImage);
-// var currentSpeciesIncrement = 0;
-// function nextImage(e) {
-//     localSpeciesService.getSpeciesByIndex(currentSpeciesIncrement);
-//     console.log(currentSpeciesIncrement);
-//     currentSpeciesIncrement++;
-// }
+//CODE USED FOR CURATING
+document.addEventListener('keypress', nextImage);
+var currentSpeciesIncrement = 0;
+function nextImage(e) {
+    localSpeciesService.getSpeciesByIndex(currentSpeciesIncrement);
+    console.log(currentSpeciesIncrement);
+    currentSpeciesIncrement++;
+}
